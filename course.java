bf01106699c80492ad275989e20197a948933ce0
Bipransh <bipransh.mehrotra@amdocs.com>
2021-09-20 15:05:01 +0530

    Second commit

---
+++ course.java
@@ -0,0 +1,7 @@
+public class course
+{
+public static void main( String args[]){
+ System.out.println("hello new course added");
+}
+}
+
--- customer.java
+++ customer.java
@@ -2,5 +2,6 @@ public class customer
 {
 public static void main( String args[]){
  System.out.println("hello i am your customer");
+ System.out.println("I wanna buy a product");
 }
 }
